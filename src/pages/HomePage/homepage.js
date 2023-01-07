ef2bde4938e2be5ce600e6ab2c8da045916baf46
Maks Skomorokha <91227382+connars@users.noreply.github.com>
2023-01-07 21:08:21 +0200

    add

--- src/pages/HomePage/homepage.js
+++ src/pages/HomePage/homepage.js
@@ -6,7 +6,7 @@ function Homepage() {
     return (
         <>
          <div className='homepage__wrapper'>
-            <div className='container'> 
+            <div id='hero' className='container'> 
                 <div className='hero'>
                     <div className='hero__left'>
                         <h1>Конвертер валют</h1>
